078eb3a9be3b1e8d095a21a8fc531a2cb417d7e9
BatchClaim <48913941+yujunweng@users.noreply.github.com>
2020-12-31 14:03:54 +0800

    Update HouseClaim.py
    add function of examine area

--- HouseClaim.py
+++ HouseClaim.py
@@ -2,84 +2,251 @@ import pandas as pd
 import numpy as np
 import os
 import traceback
+import re
 
-def examin(dataFrame, column, column_name, length, lower_equal, necessary, exam_column, type, length2 = None):
-#參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2), 第2種長度(針對IDN_BAN有2種長度)
-	df = dataFrame
+# iloc[列, 行]
+
+def writeCsv(file_name, column_name, row_num, seq, wrong_type=None):
+	# 存檔路徑
+	file_name = str(filename).split(".")[0]
+	sFilepath = "./%s.csv" %file_name
 	
+	# 開啟要寫入的檔案
+	f = open(sFilepath, 'a+', encoding = 'UTF-8', newline ='')
+	f_writer = csv.writer(f, dialect = 'excel')
+	
+	wrong_dict = {}
+	fields = ['序號', '欄位名稱', '位置', '錯誤型態', '正確型態']
+
+	wrong_dict = {'序號':seq, '欄位名稱':column_name, '位置':row_num, '錯誤型態':wrong_type, '正確型態':None}
+	if seq == 1:
+		f_writer.writerow(wrong_dict.keys())
+	f_writer.writerow(wrong_dict.values())
+	
+	# 關閉檔案
+	f.close()
+
+def writeText(file_name, row_num, hou_losn, card, df_area, data_area, error_seq):
+	# 存檔路徑
+	file_name = str(file_name).split(".")[0]
+	sFilepath = "./%s.txt" %file_name
+	
+	# 如果檔案已存在就刪除
+	if os.path.isfile(sFilepath) and error_seq == 1: 
+		os.remove(sFilepath)
+	
+	# 開啟要寫入的檔案
+	f = open(sFilepath, 'a+', encoding = 'ANSI', newline ='')
+	f.write("%d %s%d面積不同! 稅號%s, 卡序%s, 課稅主檔面積:%s, 公共設施檔面積:%s\n" %(error_seq, 'U', row_num, hou_losn, card, df_area, data_area))
+	
+	# 關閉檔案
+	f.close()
+
+def examin(dataFrame, column, column_name, length, lower_equal, necessary, exam_column, type_, length2 = None):
+# 參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2), 第2種長度(針對IDN_BAN有2種長度)
+	df = dataFrame
+
 	lengthCount = 0
 	emptyCount = 0
 	count  = 0
 
 	i = 0
-	while(i < df.shape[0]-1):
-		item = df.iloc[i][column]
+	while i < df.shape[0]-1:
+		item = df.iloc[i, column]
+		
 		if not pd.isnull(item):
-			if (type == 1):
-				if (length2 is None):
-					if (lower_equal == 'equal'):
-						if(len(str(item)) < length):
-							print("位置:%s%d, %s 字數%d,少於%d!" %(column_name, i+4, str(item), len(str(item)), length))
+			# type_ = 1 文字型態
+			if type_ == 1:
+				item = str(item).replace(".0", "")
+				if length2 is None:
+					if lower_equal == 'equal':
+						if len(item) < length:
+							print("位置:%s%d, %s 字數%d,少於%d!" %(column_name, i+4, item, len(item), length))
 							lengthCount += 1
 							
-						elif(len(str(item)) > length):
-							print("位置:%s%d, %s 字數%d,多於%d!" %(column_name, i+4, str(item), len(str(item)), length))
+						elif len(item) > length:
+							print("位置:%s%d, %s 字數%d,多於%d!" %(column_name, i+4, item, len(item), length))
 							lengthCount += 1        
 					else:
-						if(len(str(item)) > length):
-							print("位置:%s%d, %s 字數%d,多於%d!" %(column_name, i+4, str(item), len(str(item)), length))
+						if len(item) > length:
+							print("位置:%s%d, %s 字數%d,多於%d!" %(column_name, i+4, item, len(item), length))
 							lengthCount += 1
-				else:#針對IDN有2種長度
-					if(len(str(item)) != length and len(str(item)) !=length2):
-						print("位置:%s%d, %s 字數%d, 不符合長度%d或%d" %(column_name, i+4, str(item), len(str(item)), length, length2))
-			elif(type == 0):
-				if (int(item) == float(item)):
-					if (lower_equal == 'equal'):
-						if(len(str(int(item))) < length):
+				
+				else:	# 針對IDN有2種長度
+					if len(item) != length and len(item) != length2:
+						print("位置:%s%d, %s 字數%d, 不符合長度%d或%d" %(column_name, i+4, str(item), len(item), length, length2))
+			
+			# type_ = 0 整數型態
+			elif type_ == 0:
+				if int(item) == float(item):
+					if lower_equal == 'equal':
+						if len(str(int(item))) < length:
 							print("位置:%s%d, %s 字數%d,少於%d!" %(column_name, i+4, str(item), len(str(int(item))), length))
 							lengthCount += 1
-						elif(len(str(int(item))) > length):
+						elif len(str(int(item))) > length:
 							print("位置:%s%d, %s 字數%d,多於%d!" %(column_name, i+4, str(item), len(str(int(item))), length))
 							lengthCount += 1
-					elif(lower_equal == 'lower'):
-						if(len(str(int(item))) > length):
+					elif lower_equal == 'lower':
+						if len(str(int(item))) > length:
 							print("位置:%s%d, %s 字數%d,多於%d!" %(column_name, i+4, str(item), len(str(int(item))), length))
 							lengthCount += 1
 				else:
 					print("位置:%s%d, %s可能有小數!" %(column_name, i+4, str(item)))
 					lengthCount += 1
-			elif(type == 2):
-				if (lower_equal == 'equal'):
-					if(len(str(item)) < length):
+			
+			# type_ = 2 小數型態
+			elif type_ == 2:
+				if lower_equal == 'equal':
+					if len(str(item)) < length:
 						print("位置:%s%d, %s 字數%d,少於%d!" %(column_name, i+4, str(item), len(str(int(item))), length))
 						lengthCount += 1
-					elif(len(str(item)) > length):
+					elif len(str(item)) > length:
 						print("位置:%s%d, %s 字數%d,多於%d!" %(column_name, i+4, str(item), len(str(int(item))), length))
 						lengthCount += 1
-				elif(lower_equal == 'lower'):
-					if(len(str(item)) > length):
+				
+				elif lower_equal == 'lower':
+					if len(str(item)) > length:
 						print("位置:%s%d, %s 字數%d,多於%d!" %(column_name, i+4, str(item), len(str(int(item))), length))
 						lengthCount += 1		
 						
-		if(pd.isnull(item) and necessary == 1):
+		if pd.isnull(item) and necessary == 1:
 			print("%s%d不可空白!" %(column_name, i+4))
 			emptyCount += 1
 		i += 1
 	count = lengthCount+emptyCount
 	
-	if(count > 0):
+	if count > 0:
 		print("%s錯誤,共%d筆!\n" %(exam_column, count))
 		os.system("pause")
-	return count 
+	return count
+
+def examArea(file_name):
+	try:
+		# step1:存取公共設施sheet資料
+		df1 = pd.read_excel(files[readFile-1], sheet_name = '公共設施', index_col = None, header = 6, skiprows=0)
 		
+		# data 的欄位名稱
+		data = []
+		data.append([])
+		data[0] = ['稅號', '地址']		
+		columnNum = 0
+		while pd.notnull(df1.iloc[1, 5+columnNum]):
+			# 取得所有卡序名稱
+			card = df1.iloc[1, 5+columnNum]		# df1.iloc 的位置從 header 開始往下數
+			result = re.search("[A-Z]", card).span() 	# 正則表達式 找出卡序
+			#print(card[result[0]:result[1]])
+			if (int(card[0:result[0]]) < 990 and card[result[0]:result[1]+1] not in ('P', 'Y', 'Z')):	# 如果樓層<990 且 卡序不是 PYZ		
+				card = card[result[0]:result[1]+1]	# 只抓卡不抓樓層
+			data[0].append(card)
+			#print(data[0])	
+			columnNum += 1
+			
+		#os.system("pause")
+		
+		# areaData 放地址和面積
+		areaData = []	
+		i = 0
+		while (i < df1.shape[0]-4):	# shape 大小要把 header 之前的都算進去
+			areaData.append([])
+			
+			# append 地址
+			for dash in ('-', '—'):	
+				if str(df1.iloc[i+4, 1]).find(dash) > -1:
+					item1 = str(df1.iloc[i+4, 1]).replace(dash, '之')
+					break
+				else:
+					item1 = str(df1.iloc[i+4, 1])
+			areaData[i].append(item1)	
+			
+			# append 各卡序面積
+			for j in range(0, columnNum):
+				areaData[i].append(df1.iloc[i+4, 5+j])	
+	
+			#print("areaData:", areaData[i])
+			i += 1
+		
+		# step2:讀取 JRCF020坐落地址檔
+		df2 = pd.read_excel(files[readFile-1], sheet_name = 'JRCF020坐落地址檔', index_col = None, header = 2, skiprows=0,
+							dtype = {'稅籍編號':str, '號':str, '號之':str, '之':str, '之號':str, '樓':str, '樓之':str})	
+		# data 記錄稅號與地址
+		k = 0	# k 從 header 之後開始算
+		while (k < df2.shape[0]-1):
+			data.append([])
+			data[k+1].append(df2.iloc[k, 0])	# 第0列放欄位名稱
+			#print(data)
+			#os.system("pause")
+			if pd.notnull(df2.iloc[k, 10]):
+				addr = str(df2.iloc[k, 10]) + "號"
+				if pd.notnull(df2.iloc[k, 11]): 
+					addr = addr + "之" + str(df2.iloc[k, 11])	# M號之N
+
+			elif pd.notnull(df2.iloc[k, 12]):
+				addr = str(df2.iloc[k, 12]) + "之"
+				if pd.notnull(df2.iloc[k, 13]):
+					addr = addr + str(df2.iloc[k, 13]) + "號"	# M之N號
+
+			if pd.notnull(df2.iloc[k, 14]):
+				addr = addr + str(df2.iloc[k, 14]) + "樓"
+
+			data[k+1].append(addr)
+			#print(data[k])	
+			k += 1
+		
+		#print(data)
+		#os.system("pause")
+		#print(areaData, "\n")
+		#os.system("pause")
+		
+		for sub_data in data:
+			#print("sub_data", sub_data)
+			for sub_areaData in areaData:
+				#print("sub_areaData", sub_areaData)
+				if sub_data[1] == sub_areaData[0]:
+					secondsub = 1
+					while secondsub < len(sub_areaData):
+						sub_data.append(sub_areaData[secondsub])
+						secondsub += 1
+			#print(sub_data)
+		# step3:讀取 HOUF110課稅主檔,111加減項檔
+		df3 = pd.read_excel(files[readFile-1], sheet_name = 'HOUF110課稅主檔,111加減項檔', index_col = None, header = 2, skiprows=0,
+					dtype = {'稅籍編號':str, '層次':str, '卡序':str})
+		
+		p = 0	# p 從 header 之後開始算
+		error_seq = 0
+		while (p < df3.shape[0]-1):
+			#print(df3.iloc[p][0])
+			#n = input("")
+			#如果樓層<990 且 卡序不是 PYZ
+			card_seq = str(df3.iloc[p, 3]) if (int(df3.iloc[p, 2]) < 990 and df3.iloc[p, 3] not in ('P', 'Y', 'Z')) else str(df3.iloc[p, 2]) + str(df3.iloc[p, 3])
+			
+			for q in range(1, len(data)):	# data 裡面放稅號和地址
+				if df3.iloc[p, 0] == data[q][0]:	# HOUF110 的稅號比對 data 裡的稅號 
+					for r in range(2, len(data[0])):	# 找卡序
+						if card_seq == data[0][r]:	# 樓層+卡序相同
+							#print("HOUF110卡序", card_seq, ",公共設施卡序:", data[0][r], ",課稅主檔面積:", df3.iloc[p, 20], ",公共設施:", data[q][r])
+							if df3.iloc[p, 20] != data[q][r]:	# 比對面積 不同就印出欄位
+								error_seq += 1
+								print("%s%d面積不同! 稅號%s, 卡序%s, 課稅主檔面積:%s, 公共設施檔面積:%s" %('U', p+4, df3.iloc[p, 0], card_seq, df3.iloc[p, 20], data[q][r]))
+								writeText(file_name, p+4, df3.iloc[p, 0], card_seq, df3.iloc[p, 20], data[q][r], error_seq)
+							else:	# 面積相同 break 換下一筆
+								break
+			p += 1	
+		del data
+		if error_seq == 0:
+			print("面積正確!")
+		else:
+			print("\n比對結束!詳細錯誤欄位請查看%s.txt\n" %file_name.split('.')[0])
+	except:
+		traceback.print_exc()		
+
 def duplicate(column):
 	count = 0
 	i = 0
-	while(i < df.shape[0]-2):#最後一個不用再跟別人比
+	while(i < df.shape[0]-2):	# 最後一個不用再跟別人比
 		j = i+1
-		item = df.iloc[i][column]
+		item = df.iloc[i, column]
 		while(j <= df.shape[0]-1):
-			if (item == df.iloc[j][column]):
+			if (item == df.iloc[j, column]):
 				print("資料重複!A%d與A%d重複" %(i+4,j+4))
 				count += 1
 			j += 1
@@ -89,12 +256,12 @@ def duplicate(column):
 def duplicate2(column, column2):
 	count = 0
 	i = 0
-	while(i < df.shape[0]-2):#最後一個不用再跟別人比
+	while(i < df.shape[0]-2):	# 最後一個不用再跟別人比
 		j = i+1
-		item = df.iloc[i][column]
-		item2 = df.iloc[i][column2]
+		item = df.iloc[i, column]
+		item2 = df.iloc[i, column2]
 		while(j <= df.shape[0]-1):
-			if (item == df.iloc[j][column] and item2 == df.iloc[j][column2]):
+			if (item == df.iloc[j, column] and item2 == df.iloc[j, column2]):
 				print("資料重複!A%d與A%d重複" %(i+4,j+4))
 				count += 1
 			j += 1
@@ -104,15 +271,15 @@ def duplicate2(column, column2):
 def duplicate3(column, column2, column3):
 	count = 0
 	i = 0
-	while(i < df.shape[0]-2):#最後一個不用再跟別人比
+	while i < df.shape[0]-2:	# 最後一個不用再跟別人比
 		j = i+1
-		item = df.iloc[i][column]
-		item2 = df.iloc[i][column2]
-		item3 = df.iloc[i][column3]
+		item = df.iloc[i, column]
+		item2 = df.iloc[i, column2]
+		item3 = df.iloc[i, column3]
 		while(j <= df.shape[0]-1):
-			if (item == df.iloc[j][column]):
-				if (item2 == df.iloc[j][column2]):
-					if (item3 ==df.iloc[j][column3]):
+			if (item == df.iloc[j, column]):
+				if (item2 == df.iloc[j, column2]):
+					if (item3 == df.iloc[j, column3]):
 						print("資料重複!A%d與A%d重複" %(i+4,j+4))
 						count += 1
 			j += 1
@@ -121,36 +288,42 @@ def duplicate3(column, column2, column3):
 
 def specialrate():
 	i = 0
-	while(i < df.shape[0]-1):
-		if(df.iloc[i][25] == 'Y' and df.iloc[i][51] != '2'):
+	while i < df.shape[0]-1:
+		if(df.iloc[i, 25] == 'Y' and df.iloc[i, 51] != '2'):
 			print("Z%d與AZ%d加重課稅註記與稅率不符" %(i+3, i+3))
-		if(df.iloc[i][25] == 'N' and df.iloc[i][51] != '1'): 
+		if(df.iloc[i, 25] == 'N' and df.iloc[i, 51] != '1'): 
 			print("Z%d與AZ%d加重課稅註記與稅率不符" %(i+3, i+3))
 		i += 1
 
-def existHOU(df, hou_list):
+def existHou(df, hou_list):
 	i = 0
 	count = 0
-	while (i < df.shape[0]-1):
-		item = (df.iloc[i][0])
+	
+	while i < (df.shape[0]-1):
+		item = df.iloc[i, 0]
+		#print(item)
 		if item not in hou_list:
-			print("位置:A%d, 稅號%s不存在HOUF100" %(i+4, item))
+			print("位置:A%d, 稅號%s不存在HOUF100" %(i, item))
 			count += 1
 		i += 1
-	if (count > 0):
+	
+	if count > 0:
 		print("稅號不存在HOUF100共%d筆!\n" %count)
+	
 	return count
 
-def listFiles(filetype = "xls", path = os.getcwd(), massage = None):
+def listFiles(filetype_ = "xls", path = os.getcwd(), massage = None):
 	files = os.listdir(path)
 	qualifyFiles = []
+
 	if (massage != None):
 		print(massage)
+	
 	for f in files:
-		#要給完整路徑才能正確讀取
+		# 要給完整路徑才能正確讀取
 		allPath = os.path.join(path,f)
 		if (os.path.isfile(allPath)):
-			if (f[f.find(".")+1:] == filetype):
+			if (f[f.find(".")+1:] == filetype_):
 				qualifyFiles.append(f)       
 	for compareF, i in zip(qualifyFiles, range(1,len(qualifyFiles)+1)):
 		print("(%d)%s" %(i, compareF)) 
@@ -167,28 +340,28 @@ try:
 		#print(df.tail())
 
 		##列出檔案供選擇
-		files = listFiles(filetype = "xls")
+		files = listFiles(filetype_ = "xls")
 		readFile = int(input("請選擇檔案："))
 		print("\n")
-
-
+		
 		##HOUF100
-		df = pd.read_excel(files[readFile-1], sheet_name = 'HOUF100中文主檔', index_col = None, header = 2, skiprow = 2,
-							dtype={'稅籍編號':str, '公私有別':str, '歸屬別':str, '設籍文號':str, '設籍日期':str, '使用執照編號':str})
+		df = pd.read_excel(files[readFile-1], sheet_name = 'HOUF100中文主檔', index_col = None, header = 2, skiprows = 0,
+							dtype = {'稅籍編號':str, '公私有別':str, '歸屬別':str, '設籍文號':str, '設籍日期':str, '使用執照編號':str})
 
 		print("\n開始檢查HOUF100...")
 		columns = df.columns
-		#抓所有稅號，用來檢查其他表的稅號是否存在HOUF100
+		
+		# 抓所有稅號，用來檢查其他表的稅號是否存在HOUF100
 		hou = []
 		i = 0
-		while (i < df.shape[0]):
-			hou.append(df.iloc[i][0])
+		while i < (df.shape[0]-1):
+			hou.append(df.iloc[i, 0])
 			i += 1
 
-		#檢查稅號是否重複
+		# 檢查稅號是否重複
 		duplicate(0)
 
-		#參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(數字：0, 其他：1)
+		# 參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(數字：0, 文字：1, 小數：2)
 		examin(df, 0, 'A', 11, 'equal', 1, '稅籍編號', 1)
 		examin(df, 1, 'B', 1, 'equal', 1, '公私有別', 1)
 		examin(df, 2, 'C', 1, 'equal', 0, '歸屬別', 1)
@@ -199,24 +372,24 @@ try:
 
 		##JRCF020坐落地址檔
 		sheet = 'JRCF020坐落地址檔'
-		df = pd.read_excel(files[readFile-1], sheet_name = sheet, index_col = None, header = 2, skiprow = 2,
+		df = pd.read_excel(files[readFile-1], sheet_name = sheet, index_col = None, header = 2, skiprows = 0,
 							dtype = {'稅籍編號':str, '郵遞區號':str, '縣市鄉鎮村里':str, '鄰':str, '路街':str, '段':str, '巷':str,
 									 '中文巷':str, '弄':str, '衖':str, '號':str, '號之':str, '之':str, '之號':str, '樓':str, '樓之':str,
 									 '室':str, '房屋座落':str, '特殊地址註記':str})
 		print("\n開始檢查JRCF020")
 
-		#檢查稅號是否存在HOUF100
-		errorhou_count += existHOU(df, hou)
+		# 檢查稅號是否存在HOUF100
+		errorhou_count += existHou(df, hou)
 
-		#檢查稅號是否重複
+		# 檢查稅號是否重複
 		duplicate(0)
 
-		#檢查結尾*字號
-		if(df.iloc[df.shape[0]-1][0] != '*'):
+		# 檢查結尾*字號
+		if df.iloc[df.shape[0]-1, 0] != '*':
 			print("結尾沒有*")
 			print("\n")
 
-		#參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2)  
+		# 參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2)  
 		examin(df, 0, 'A', 11, 'equal', 1, '房屋稅籍編號', 1)        
 		examin(df, 1, 'B', 3, 'equal', 0, '郵遞區號', 0)
 		examin(df, 2, 'C', 5, 'equal', 1, '縣市鄉鎮村里', 1)
@@ -237,7 +410,7 @@ try:
 
 
 		##HOUF110課稅主檔,111加減項檔
-		df = pd.read_excel(files[readFile-1], sheet_name = 'HOUF110課稅主檔,111加減項檔', index_col = None, header = 2, skiprow = 2,
+		df = pd.read_excel(files[readFile-1], sheet_name = 'HOUF110課稅主檔,111加減項檔', index_col = None, header = 2, skiprows = 0,
 							dtype = {'稅籍編號':str, '使照編號':str, '層次':str, '卡序':str, '建物類別':str,
 									 '公設名稱':str, '構造別':str, '用途類別':str, '用途細類':str, '總層數':str,
 									 '標準單價':str, '樓層高度':str, '核定單價':str, '折舊率':str, '折舊率序號':str,
@@ -249,13 +422,13 @@ try:
 									 '分層分攤率':str, '稅率代號':str, '使用別':str})
 		print("\n開始檢查HOUF110課稅主檔,111加減項檔")
 
-		#檢查稅號是否存在HOUF100
-		errorhou_count += existHOU(df, hou)
+		# 檢查稅號是否存在HOUF100
+		errorhou_count += existHou(df, hou)
 
-		#檢查稅號+層次+卡序是否重複
+		# 檢查稅號+層次+卡序是否重複
 		duplicate3(0, 2, 3)
 
-		#參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2), 第2種長度(針對IDN_BAN有2種長度)
+		# 參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2), 第2種長度(針對IDN_BAN有2種長度)
 		examin(df, 0, 'A', 11, 'equal', 1, '稅籍編號', 1)
 		examin(df, 1, 'B', 14, 'equal', 0, '使照編號', 1)
 		examin(df, 2, 'C', 3, 'lower', 1, '層次', 0)
@@ -310,12 +483,12 @@ try:
 		examin(df, 51, 'AZ', 1, 'equal', 1, '稅率代號', 0)
 		examin(df, 52, 'BA', 1, 'equal', 0, '使用別', 0)
 
-		#檢查加重課稅註記與稅率是否相符
+		# 檢查加重課稅註記與稅率是否相符
 		specialrate()
-
+		
 
 		##HOUF120持分人檔
-		df = pd.read_excel(files[readFile-1], sheet_name = 'HOUF120持分人檔', index_col = None, header = 2, skiprow = 2,
+		df = pd.read_excel(files[readFile-1], sheet_name = 'HOUF120持分人檔', index_col = None, header = 2, skiprows = 0,
 							dtype = {'稅籍編號':str, '移轉日期':str, '卡別':str, '群組':str, '持分分子':str, 
 									 '持分分母':str, '課稅月數':str, '是否分單':str, '是否分管':str, 'IDN_BAN':str, 
 									 '身分代號':str, '姓   名':str, '備註':str, '通 訊  地  址':str, '郵遞區號':str, 
@@ -324,13 +497,13 @@ try:
 									 '室':str, '特殊地址註記':str})
 		print("\n開始檢查HOUF120持分人檔")
 
-		#檢查稅號是否存在HOUF100
-		errorhou_count += existHOU(df, hou)
+		# 檢查稅號是否存在HOUF100
+		errorhou_count += existHou(df, hou)
 
-		#檢查稅號+卡別是否重複
+		# 檢查稅號+卡別是否重複
 		duplicate2(0, 2)
 
-		#參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2), 第2種長度(針對IDN_BAN有2種長度)
+		# 參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2), 第2種長度(針對IDN_BAN有2種長度)
 		examin(df, 0, 'A', 11, 'equal', 1, '稅籍編號', 1)
 		examin(df, 1, 'B', 7, 'equal', 0, '移轉日期', 1)
 		examin(df, 2, 'C', 1, 'equal', 1, '卡別', 1)
@@ -348,19 +521,24 @@ try:
 
 		##HOUF151土地標示檔
 
-		df = pd.read_excel(files[readFile-1], sheet_name = 'HOUF151土地標示檔', index_col = None, header = 2, skiprow = 2,
+		df = pd.read_excel(files[readFile-1], sheet_name = 'HOUF151土地標示檔', index_col = None, header = 2, skiprows = 0,
 							dtype = {'稅籍編號':str, '土地標示':str})
 		print("\n開始檢查HOUF151土地標示檔")
 
-		#檢查稅號是否存在HOUF100
-		errorhou_count += existHOU(df, hou)
+		# 檢查稅號是否存在HOUF100
+		errorhou_count += existHou(df, hou)
 
-		#檢查稅號+土標是否重複
+		# 檢查稅號+土地標示是否重複
 		duplicate2(0, 1)
 
-		#參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2), 第2種長度(針對IDN_BAN有2種長度)
+		# 參數：欄位, 欄位名稱, 字串長度, 是否可小於字串長度(equal,lower), 是否必填(是：1, 否：0), 欄位中文, 資料型態(整數：0, 文字：1, 小數：2), 第2種長度(針對IDN_BAN有2種長度)
 		examin(df, 0, 'A', 11, 'equal', 0, '稅籍編號', 1)
 		examin(df, 1, 'B', 14, 'equal', 0, '土地標示', 1)
+		
+		
+		# 檢查面積
+		examArea(files[readFile-1])
+
 except:
 	traceback.print_exc()
 
